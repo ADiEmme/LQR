37b00fe17e5248333c660ebb2eb7e25504e77cbf
Andrea <delmonaco.andrea@gmail.com>
2018-10-26 11:01:20 +0200

    Improved algorithm

--- lqr-tool.py
+++ lqr-tool.py
@@ -2,6 +2,8 @@
 import sys
 import random
 import ast
+import itertools
+import pprint
 
 def main():
   print ('''######## Letters quest remastered help tool ########\n# Criteria:\n# - The longer the word is, the more points you will get.\n# - Double letters give more points.\n####################################################''')
@@ -9,26 +11,17 @@ def main():
   anagram(letters)
 
 def anagram(letters):
-  shuf_array=[]
-  shuffled=''.join(random.sample(letters, len(letters)))
+  shuf_array=list(map("".join, itertools.permutations(letters)))
   dict_res=[]
-  while shuffled not in shuf_array:
-    #shuffled = ''.join(random.sample(letters, random.randrange(3,len(letters))))
-    shuf_array.append(shuffled)
-    with open('/home/adiemme/LQR/words') as f:
-        for line in f:
-          line = line.strip()
-          if line in shuf_array[-1]:
-            #print line+" = "+shuf_array[-1]
-            dict_res.append(line)
-    shuffled = ''.join(random.sample(letters, len(letters)))
-    print shuf_array
-    while shuffled in shuf_array:
-        shuffled = ''.join(random.sample(letters, len(letters)))
-        print shuffled
-    #shuf_array.append(shuffled)
- 
-  dict_res.sort(key=len, reverse=True)
+  for shuf in shuf_array:
+    with open('words') as f:
+      for line in f:
+        line = line.strip().lower()
+        if line == shuf:
+          dict_res.append(line)
+  
+  dict_res=list(set(dict_res))
   print dict_res
+
 main ()
 
