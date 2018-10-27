5141cd2daf14c61071d635fe3230c924b7fd1ff5
Andrea <delmonaco.andrea@gmail.com>
2018-10-28 00:55:52 +0200

    Match if modified

--- lqr-tool.py
+++ lqr-tool.py
@@ -3,24 +3,26 @@ import sys
 import random
 import ast
 import itertools
-import pprint
 
 def main():
   print ('''######## Letters quest remastered help tool ########\n# Criteria:\n# - The longer the word is, the more points you will get.\n# - Double letters give more points.\n####################################################''')
   letters=sys.argv[1]
   anagram(letters)
 
+
 def anagram(letters):
   shuf_array=list(map("".join, itertools.permutations(letters)))
   dict_res=[]
+  #print shuf_array
   for shuf in shuf_array:
     with open('words') as f:
       for line in f:
         line = line.strip().lower()
-        if line == shuf:
+        if line in shuf or shuf in line:
           dict_res.append(line)
   
-  dict_res=list(set(dict_res))
+  dict_res=sorted(list(set(dict_res)), key=len, reverse=True)
+
   print dict_res
 
 main ()
