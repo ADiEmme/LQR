4bd4cfeab469e7da53728778bda8ff4ccf831c3b
Andrea <delmonaco.andrea@gmail.com>
2018-10-28 02:02:44 +0200

    Multiprocessing support added

--- lqr-tool.py
+++ lqr-tool.py
@@ -3,6 +3,11 @@ import sys
 import random
 import ast
 import itertools
+import multiprocessing as mp
+
+
+output = mp.Queue()
+
 
 def main():
   print ('''######## Letters quest remastered help tool ########\n# Criteria:\n# - The longer the word is, the more points you will get.\n# - Double letters give more points.\n####################################################''')
@@ -11,19 +16,38 @@ def main():
 
 
 def anagram(letters):
-  shuf_array=list(map("".join, itertools.permutations(letters)))
-  dict_res=[]
-  #print shuf_array
-  for shuf in shuf_array:
-    with open('words') as f:
-      for line in f:
-        line = line.strip().lower()
-        if line in shuf or shuf in line:
-          dict_res.append(line)
+  shuf_array_big=list(map("".join, itertools.permutations(letters)))
+  dict_res=[] 
+  for i in range(0,len(shuf_array_big),8):  
+    jobs = []
+    shuf_array=shuf_array_big[i:i+8]
+    #print shuf_array
+    for shuf in shuf_array:
+      processes = mp.Process(target=findit, args=(shuf,))
+      jobs.append(processes)
   
-  dict_res=sorted(list(set(dict_res)), key=len, reverse=True)
+    # Run processes
+    for p in jobs:
+      p.start()
 
+    # Exit the completed processes
+    for p in jobs:
+      p.join()
+
+    # Get process results from the output queue
+    results = [output.get() for p in jobs]
+
+    dict_res.append(results)
+
+  dict_res=sorted(set(list(itertools.chain.from_iterable(dict_res))), key=len, reverse=True)
   print dict_res
+
+def findit(shuf):
+  with open('words') as f:
+    for line in f:
+      line = line.strip().lower()
+      if line in shuf or shuf in line:
+        output.put(line)
 
 main ()
 
